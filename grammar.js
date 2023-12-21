358064ec22346760b23b0ea6abaf91f025b6e2ad
KITAGAWA Yasutaka <kit494way@gmail.com>
2023-12-21 21:14:25 +0900

    Fix typo

--- grammar.js
+++ grammar.js
@@ -945,7 +945,7 @@ module.exports = grammar({
 /// To correctly parse these situations distinct rules for different types of
 /// statements are needed. These rules are differentiated by suffix, and only
 /// difference between them is terminator parameter used in pipeline rule that
-/// is terminating statements. This function automaticaly generates all rules
+/// is terminating statements. This function automatically generates all rules
 /// for a given terminator and names them with specified suffix.
 function block_body_rules(suffix, terminator) {
   function alias_for_suffix($, rule_name, suffix) {
