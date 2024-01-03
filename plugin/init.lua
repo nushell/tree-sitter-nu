b2065c569cda7dac35d5163c482d82f3959a4366
aserowy <serowy@hotmail.com>
2024-01-03 07:25:35 +0100

    removed test url for parser
    Signed-off-by: aserowy <serowy@hotmail.com>

--- plugin/init.lua
+++ plugin/init.lua
@@ -7,7 +7,7 @@ vim.api.nvim_create_autocmd("FileType", {
 
 require("nvim-treesitter.parsers").get_parser_configs().nu = {
   install_info = {
-    url = "https://github.com/aserowy/tree-sitter-nu",
+    url = "https://github.com/nushell/tree-sitter-nu",
     files = { "src/parser.c" },
     branch = "main",
   },
