c37b8250a60fa671d61cbc25976b6bc89cfcac00
Max Brunsfeld <maxbrunsfeld@gmail.com>
2024-04-12 15:58:32 -0700

    Regenerate with latest tree-sitter-cli

---
+++ bindings/swift/TreeSitterNu/nu.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_NU_H_
+#define TREE_SITTER_NU_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_nu(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_NU_H_
