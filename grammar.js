c7869fc16099a3df13f1157627edec0f8fd3e44e
CabalCrow <echizhov@disroot.org>
2024-05-23 23:51:42 +0300

    Format with prettier.

--- grammar.js
+++ grammar.js
@@ -118,7 +118,10 @@ module.exports = grammar({
         seq(
           optional(MODIFIER().visibility),
           KEYWORD().use,
-          field("module", choice($.val_string, $.expr_parenthesized, $.unquoted)),
+          field(
+            "module",
+            choice($.val_string, $.expr_parenthesized, $.unquoted),
+          ),
           optional(field("import_pattern", $.scope_pattern)),
         ),
       ),
