71a66e46229a8e49d329952edb6075521c360815
Maxim Onciul <onciul@cs.fau.de>
2024-09-17 16:48:13 +0200

    chore: (hopefully) commit all automatically created/modified files

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_nu_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-nu"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_nu "github.com/tree-sitter/tree-sitter-nu/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
