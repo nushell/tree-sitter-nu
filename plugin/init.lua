657dcb67b18607559f2e72266f6e8008cae97c2c
mrdgo <vmaxi1@ymail.com>
2024-09-22 11:20:31 +0200

    fix: init.lua

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,14 +1,17 @@
 vim.filetype.add({ extension = { nu = "nu" }, filename = { ["nurfile"] = "nu" } })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "nu",
-  callback = function(event) vim.bo[event.buf].commentstring = "# %s" end,
+	pattern = "nu",
+	callback = function(event)
+		vim.bo[event.buf].commentstring = "# %s"
+	end,
 })
 
 require("nvim-treesitter.parsers").get_parser_configs().nu = {
-  install_info = {
-    url = "https://github.com/nushell/tree-sitter-nu",
-  },
-  filetype = "nu",
+	install_info = {
+		url = "https://github.com/nushell/tree-sitter-nu",
 		files = { "src/parser.c", "src/scanner.c" },
+		branch = "main",
+	},
+	filetype = "nu",
 }
