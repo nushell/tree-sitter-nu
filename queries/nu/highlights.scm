3a021cd7902d8e3163283efd58613d07cd07c5cd
mrdgo <vmaxi1@ymail.com>
2024-11-28 20:34:56 +0100

    feat: raw strings: distinguish delimiters from actual string (#114)
    We could set the delimiters to a different highlight group. Example
    `@punctuation.special`:
    
    ![image](https://github.com/user-attachments/assets/4903f641-48fe-42f4-87dd-6245c644447d)
    
    To see all available groups, hit `:h treesitter-highlight-groups`. I
    could see:
    - `@punctuation.special`
    - `@punctuation.delimiter`
    - `@tag.delimiter`
    - `@string.escape`
    - `@string.special`
    - `@string.special.symbol`
    
    Any opinions?
    
    Edit: with highlights now in `nvim-treesitter`, this feels a bit weird.
    But we can still maintain our own highlights :D

--- queries/nu/highlights.scm
+++ queries/nu/highlights.scm
@@ -90,6 +90,10 @@ file_path: (val_string) @variable.parameter
 (escaped_interpolated_content) @string
 (expr_interpolated ["(" ")"] @variable.parameter)
 
+(raw_string_begin) @punctuation.special
+(raw_string_content) @string
+(raw_string_end) @punctuation.special
+
 ;;; ---
 ;;; operators
 (expr_binary [
