af5af3a63a5c22c7d54dc0211b56306cb5bceb77
Bahex <17417311+Bahex@users.noreply.github.com>
2025-07-03 22:00:40 +0300

    refactor: simplify queries and reformat
    Also add `@string.code` to injected `nu` code.

--- queries/nu/injections.scm
+++ queries/nu/injections.scm
@@ -1,39 +1,35 @@
 ((comment) @injection.content
- (#set! injection.language "comment"))
+  (#set! injection.language "comment"))
 
-((command
- head: ((cmd_identifier) @_cmd (#any-of? @_cmd
-  "find"
-  "parse"
-  "split"
-  "str"
-  " find"
-  " parse"
-  " split"
-  " str"
- ))
- flag: [
-  (short_flag name: (_) @_sflag (#eq? @_sflag "r"))
-  (long_flag name: (_) @_lflag (#eq? @_lflag "regex"))
- ]
- .
- arg: (_ (string_content) @injection.content)
- (#set! injection.language "regex")
-))
+(command
+  head: ((cmd_identifier) @_cmd
+    (#match? @_cmd "^\\s*(find|parse|split|str)$"))
+  flag: (_
+    name: (_) @_flag
+    (#any-of? @_flag "r" "regex"))
+  .
+  arg: (_
+    (string_content) @injection.content
+    (#set! injection.language "regex")))
 
-((_
- opr: ["=~" "!~" "like" "not-like"]
- rhs: (_ (string_content) @injection.content)
- (#set! injection.language "regex")
-))
+(_
+  opr: [
+    "=~"
+    "!~"
+    "like"
+    "not-like"
+  ]
+  rhs: (_
+    (string_content) @injection.content
+    (#set! injection.language "regex")))
 
-((command
- head: (_) @_cmd (#any-of? @_cmd "nu" "$nu.current-exe")
- flag: [
-  (short_flag name: (_) @_sflag (#any-of? @_sflag "c" "e"))
-  (long_flag name: (_) @_lflag (#any-of? @_lflag "commands" "execute"))
- ]
- .
- arg: (_ (string_content) @injection.content)
- (#set! injection.language "nu")
-))
+(command
+  head: (_) @_cmd
+  (#any-of? @_cmd "nu" "$nu.current-exe")
+  flag: (_
+    name: (_) @_flag
+    (#any-of? @_flag "c" "e" "commands" "execute"))
+  .
+  arg: (_
+    (string_content) @injection.content
+    (#set! injection.language "nu")))
