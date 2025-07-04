436b3f94578974d6e0c89c201d4b99329ad0e944
Bahex <Bahex@users.noreply.github.com>
2025-07-04 11:57:33 +0300

    improve language injections (#211)
    - refactor: simplify queries and reformat
    - allow additional formatting with `@string.code`

--- queries/nu/highlights.scm
+++ queries/nu/highlights.scm
@@ -276,26 +276,32 @@ key: (identifier) @property
 (parameter
   (comment) @comment.documentation @spell)
 
-((command
-    head: ((cmd_identifier) @_cmd (#any-of? @_cmd
-        "find"
-        "parse"
-        "split"
-        "str"
-        " find"
-        " parse"
-        " split"
-        " str"
-    ))
-    flag: [
-        (short_flag name: (_) @_sflag (#eq? @_sflag "r"))
-        (long_flag name: (_) @_lflag (#eq? @_lflag "regex"))
-    ]
-    .
-    arg: (_ (string_content) @string.regexp)
-))
+(command
+  head: ((cmd_identifier) @_cmd
+    (#match? @_cmd "^\\s*(find|parse|split|str)$"))
+  flag: (_
+    name: (_) @_flag
+    (#any-of? @_flag "r" "regex"))
+  .
+  arg: (_
+    (string_content) @string.regexp))
 
-((_
-    opr: ["=~" "!~" "like" "not-like"]
-    rhs: (_ (string_content) @string.regexp)
-))
+(_
+  opr: [
+    "=~"
+    "!~"
+    "like"
+    "not-like"
+  ]
+  rhs: (_
+    (string_content) @string.regexp))
+
+(command
+  head: ((_) @_cmd
+    (#any-of? @_cmd "nu" "$nu.current-exe"))
+  flag: (_
+    name: (_) @_flag
+    (#any-of? @_flag "c" "e" "commands" "execute"))
+  .
+  arg: (_
+    (string_content) @string.code))
