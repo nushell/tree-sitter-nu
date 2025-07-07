562fab385eae0413b3f967b8c7d4349e7d2483d7
blindfs <blindfs19@gmail.com>
2025-07-07 23:31:39 +0800

    feat(ci): eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import treesitter from 'eslint-config-treesitter';
+import prettier from 'eslint-config-prettier';
 
-export default [
-  ...treesitter,
-];
+export default [...treesitter, prettier];
