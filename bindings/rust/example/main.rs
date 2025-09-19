18aed0562f585bf27f6688d9ca29b0e185579a1f
Mikhail Katychev <mkatych@gmail.com>
2025-09-19 11:22:18 -0500

    revert deletion

---
+++ bindings/rust/example/main.rs
@@ -0,0 +1,106 @@
+extern crate tree_sitter_nu;
+use tree_sitter::{Language, Parser, TreeCursor};
+
+extern "C" {
+    fn tree_sitter_nu() -> Language;
+}
+
+fn main() {
+    let src = include_str!("../example-file.nu");
+    let nu_lang = unsafe { tree_sitter_nu() };
+    let mut parser = Parser::new();
+    parser.set_language(&nu_lang).unwrap();
+    let parse_tree = parser.parse(src, None).unwrap();
+    println!("\nTree Walk 1");
+    print_tree(&parse_tree);
+    println!("\nTree Walk 2");
+    let mut tree_cursor = parse_tree.walk();
+    walk_tree(&mut tree_cursor, src);
+    println!("\n{}", parse_tree.root_node().to_sexp());
+
+    // let query = Query::new(
+    //     nu_lang,
+    //     r#"
+    //     ((custom_command
+    //       custom_command_name: (identifier)
+    //     ))
+    //     "#
+    // ).unwrap();
+
+    // let mut query_cursor = QueryCursor::new();
+    // let all_matches = query_cursor.matches(&query, parse_tree.root_node(), src.as_bytes());
+    // println!("matches: {:?}", all_matches.count());
+}
+
+pub fn print_tree(tree: &tree_sitter::Tree) {
+    let mut cursor = tree.walk();
+    print_cursor(&mut cursor, 0);
+}
+
+fn print_cursor(cursor: &mut TreeCursor, depth: usize) {
+    loop {
+        let node = cursor.node();
+        println!("{}{:#?}", "  ".repeat(depth), node);
+
+        if cursor.goto_first_child() {
+            print_cursor(cursor, depth + 1);
+            cursor.goto_parent();
+        }
+
+        if !cursor.goto_next_sibling() {
+            break;
+        }
+    }
+}
+
+fn walk_tree(cursor: &mut TreeCursor, source: &str) {
+    // let node = cursor.node();
+    // let start_pos = node.start_position();
+    // let end_pos = node.end_position();
+    // let kind = node.kind().trim();
+    // let field_id = cursor.field_id().unwrap_or(0);
+    // let field_name = cursor.field_name().unwrap_or("");
+    // let node_string = node.utf8_text(source.as_bytes()).unwrap();
+    // println!(
+    //     "id: [{}], name: [{}], kind: [{}], start_pos: [{} {}] end_pos: [{} {}], node_text: [{}]",
+    //     field_id,
+    //     field_name,
+    //     kind,
+    //     start_pos.row,
+    //     start_pos.column,
+    //     end_pos.row,
+    //     end_pos.column,
+    //     node_string
+    // );
+
+    if cursor.goto_first_child() {
+        loop {
+            walk_tree(cursor, source);
+            let node = cursor.node();
+            let start_pos = node.start_position();
+            let end_pos = node.end_position();
+            let kind = node.kind().trim();
+            let field_id = cursor
+                .field_id()
+                .unwrap_or(core::num::NonZero::new(1).unwrap());
+            let field_name = cursor.field_name().unwrap_or("");
+            let node_string = node.utf8_text(source.as_bytes()).unwrap();
+            println!(
+                "id: [{}], name: [{}], kind: [{}], start_pos: [{} {}] end_pos: [{} {}], node_text: [{}]",
+                field_id,
+                field_name,
+                kind,
+                start_pos.row,
+                start_pos.column,
+                end_pos.row,
+                end_pos.column,
+                node_string
+            );
+
+            if !cursor.goto_next_sibling() {
+                break;
+            }
+        }
+        cursor.goto_parent();
+    }
+}
--- ts.nu
+++ ts.nu
@@ -91,16 +91,40 @@ export def "ts regen" [] {
     # https://github.com/tree-sitter/tree-sitter/blob/v0.25.9/docs/src/cli/init.md#binding-files
     (
       rm --force
-        CMakeLists.txt
-        Cargo.toml
+        # C/C++
         Makefile
-        Package.swift
-        binding.gyp
+        CMakeLists.txt
+        bindings/c/tree_sitter/tree-sitter-language.h
+        bindings/c/tree-sitter-language.pc
+        # Go
         go.mod
+        bindings/go/binding.go
+        bindings/go/binding_test.go
+        # Node
+        binding.gyp
+        package.json
+        bindings/node/binding.cc
+        bindings/node/index.js
+        bindings/node/index.d.ts
+        bindings/node/binding_test.js
+        # Python
         pyproject.toml
         setup.py
+        bindings/python/tree_sitter_language/binding.c
+        bindings/python/tree_sitter_language/__init__.py
+        bindings/python/tree_sitter_language/__init__.pyi
+        bindings/python/tree_sitter_language/py.typed
+        bindings/python/tests/test_binding.py
+
+        # Rust
+        Cargo.toml
+        bindings/rust/lib.rs
+        bindings/rust/build.rs
+        # Swift
+        Package.swift
+        bindings/swift/TreeSitterLanguage/language.h
+        bindings/swift/TreeSitterLanguageTests/TreeSitterLanguageTests.swift
     )
-    rm -rf bindings/
     tree-sitter init --update
     return
 }
